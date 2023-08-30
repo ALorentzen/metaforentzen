581f2b06c25205fee4af56c55091435ee4e90e48
Anders Lorentzen <anderslorentzen@hotmail.com>
2023-08-30 08:33:16 +0200

    home page components, about, portfolio home categories

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,8 +1,9 @@
 // Get all the .js-image elements
-const images = document.querySelectorAll('.js-image');
+const images = document.querySelectorAll('.js-individual-image');
+
 const previewer = document.querySelector('.js-previewer');
 const previewerImage = document.querySelector('.js-previewer-image');
-const closeButtons = document.querySelector('.js-close-btn');
+const closeButtons = document.querySelectorAll('.js-close-btn');
 const nextButton = document.querySelector('.js-next-btn');
 const prevButton = document.querySelector('.js-prev-btn');
 
@@ -11,64 +12,69 @@ const hamBtn = document.querySelector('.js-hamburger-button');
 const hamList = document.querySelector('.js-hamburger-list');
 const noScroll = document.querySelector('body');
 
-let currentImageIndex = 0; // Initialize the index
+// Array of indices
+const imageIndices = Array.from(images).map((_, index) => index); 
 
-// Attach click event listener to each .js-image element
-images.forEach((image, index) => {
-    noScroll.classList.toggle('noScroll');
-    
-    image.addEventListener('click', () => {
-        const imageUrl = image.querySelector('img').getAttribute('src');
+// Initialize the index
+let currentImageIndex = 0; 
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Function to show image at the given index
+    function showImage(imageUrl) {
         previewerImage.src = imageUrl;
         previewer.classList.remove('hidden');
-        currentImageIndex = index; 
+        noScroll.classList.add('noScroll');
+        navHeader.classList.toggle('hidden');
+    }
+
+    images.forEach((imageCard, index) => {
+        imageCard.addEventListener('click', () => {
+            const imageUrl = imageCard.querySelector('img').getAttribute('src');
+            showImage(imageUrl);
+        });
     });
-});
 
-// Attach click event listener to the body
-document.body.addEventListener('click', (e) => {
-    
-    if (e.target.classList.contains('js-close-btn')) {
-        previewer.classList.toggle('hidden');
+    // Attach keydown event listener to the body for Esc key
+    document.body.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            previewer.classList.add('hidden');
+            noScroll.classList.remove('noScroll');
+            navHeader.classList.toggle('hidden');
+        }
+    });
+
+    closeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            previewer.classList.add('hidden');
+            noScroll.classList.remove('noScroll');
+            navHeader.classList.toggle('hidden');
+
+        });
+    });
+
+    // Event listener for the "Next" button
+    if(nextButton) {
+        nextButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            currentImageIndex = (currentImageIndex + 1) % images.length;
+            const imageUrl = images[currentImageIndex].querySelector('img').getAttribute('src');
+            showImage(imageUrl);
+        });
+    }
+
+    // Event listener for the "Previous" button
+    if(prevButton) {
+        prevButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+            const imageUrl = images[currentImageIndex].querySelector('img').getAttribute('src');
+            showImage(imageUrl);
+        });
+    }
+
+    hamBtn.addEventListener('click', () => {
+        hamList.classList.toggle('hamburgerActive');
+        navHeader.classList.toggle('active');
         noScroll.classList.toggle('noScroll');
-    }
-});
-closeButtons.addEventListener('click', () => {
-    previewer.classList.toggle('hidden')
-});
-
-// Attach keydown event listener to the body for Esc key
-document.body.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') {
-        previewer.classList.add('hidden');
-    }
-});
-
-
-// Event listeners for next and previous buttons
-nextButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    navigateImages(1);
-});
-
-prevButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    navigateImages(-1);
-});
-
-// Navigate through the images based on the given offset
-function navigateImages(offset) {
-    currentImageIndex = (currentImageIndex + offset + images.length) % images.length;
-    const currentImage = images[currentImageIndex];
-    const imageUrl = currentImage.querySelector('img').getAttribute('src');
-    previewerImage.src = imageUrl;
-}
-
-hamBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    hamList.classList.toggle('hamburgerActive');
-    navHeader.classList.toggle('active');
-    // no scroll on body when hamburger menu is open
-    noScroll.classList.toggle('noScroll');
+    });
 });
